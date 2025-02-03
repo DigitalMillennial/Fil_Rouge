6cb70688ed54fb8c73bc9cbe36a01bdf982da1eb
DigitalMillennial <19boiko93@gmail.com>
2025-02-03 16:28:18 +0100

    Users

---
+++ src/Entity/User.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 180, nullable: true)]
+    private ?string $Email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Password = null;
+
+    #[ORM\Column]
+    private array $Roles = [];
+
+    #[ORM\Column]
+    private bool $isVerified = false;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(?string $Email): static
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->Password;
+    }
+
+    public function setPassword(string $Password): static
+    {
+        $this->Password = $Password;
+
+        return $this;
+    }
+
+    public function getRoles(): array
+    {
+        return $this->Roles;
+    }
+
+    public function setRoles(array $Roles): static
+    {
+        $this->Roles = $Roles;
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
+
+        return $this;
+    }
+}
